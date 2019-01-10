6c42646e0ab623d7e5d11412717ebfc9dd0ce745
Linya Hu <lh62594@yahoo.com>
2019-01-09 22:28:43 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,8 @@
+require 'pry'
+
+class Artist
+  
+  attr_accessor :name
+
+
+end #this is the end of the Artist class
---
+++ lib/song.rb
@@ -0,0 +1,7 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :title, :artist
+
+end #this is the end of the Song class
